7087f31b077f7b8f0dff494356f68e90cc4f25f7
Chiken3 <buraddo613@gmail.com>
2023-02-02 17:44:00 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
       #Success (valid params)
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       #Failure (not valid params)
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
     
     User.create(user_params)
